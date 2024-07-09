5fa11cf5aa8ed0a51498dfaee8dfeaa306e42853
jordansanchez23 <joivisvis@hotmail.com>
2024-07-09 15:39:11 -0700

    Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 10, 2);
+        Circle circle = new Circle("Yellow", 5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape sha in shapes)
+        {
+            string sh1 = sha.GetColor();
+            double sh2 = sha.GetArea();
+            Console.WriteLine($"{sh1}, {sh2}");
+            
+        }
+
+        // DisplayShape(square);
+        // DisplayShape(rectangle);
+        // DisplayShape(circle);
+        
+        // static void DisplayShape(Shape shape)
+        // {
+        //     Console.WriteLine(shape.GetArea());
+        // }
+        
     }
 }
